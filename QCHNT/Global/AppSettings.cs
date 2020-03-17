e07aee69c11e3232e9c57033bc329306a75012de
mazhaolei <403085447@qq.com>
2020-03-17 17:20:55 +0800

    新增全局变量类

---
+++ QCHNT/Global/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QCHNT.Global
+{
+    public class AppSettings
+    {
+        public static string DBConnection = "";
+
+    }
+}
