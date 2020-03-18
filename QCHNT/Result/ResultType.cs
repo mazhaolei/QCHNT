6b05ebcc892fbe16860d1847d7620d330e61b36b
mazhaolei <403085447@qq.com>
2020-03-18 18:30:01 +0800

    新增返回信息类型

---
+++ QCHNT/Result/ResultType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QCHNT.Result
+{
+    public partial class ResultType
+    {
+        //信息
+        public string Msg { get; set; }
+        //描述
+        public string Description { get; set; }
+    }
+}
