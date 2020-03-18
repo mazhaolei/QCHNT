2b1ac3d817b529a2ff29f5d6ade0e8717bbb3a4f
mazhaolei <403085447@qq.com>
2020-03-18 09:45:49 +0800

    功能添加

--- QCHNT/Controllers/UserController.cs
+++ QCHNT/Controllers/UserController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using QCHNT.Model;
-using System;
+using QCHNT.Result;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace QCHNT.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+
     public class UserController : ControllerBase
     {
         private readonly qchdbContext _context;
@@ -15,16 +19,39 @@ namespace QCHNT.Controllers
         {
             this._context = context;
         }
-        private readonly IHttpClientFactory _httpClient;
 
+        [HttpPost]
+        [Route("Login")]   //登录
+        public IActionResult Login([FromBody] User user)
+        {
+            LoginResultType result = new LoginResultType();
+            string sql = "select * from user where name='" + user.Name + "' and password='"+user.Password+"'";
+            List<User> list = _context.User.FromSql(sql).ToList();
+            if (list.Count==1)
+            {
+                result.Errorinfo = "true";
+                foreach(User list1 in list)
+                {
+                    result.Description = list1.Type;
+                }
+            }
+            else
 
+            {
+                result.Errorinfo = "false";
+                result.Description = "账号或密码错误";
 
+            }
+            return Json();
+        }
 
-
-
-
-
-
-
+        [HttpGet]
+        [Route("Gets")]        //获取所有用户
+        public ActionResult<string> Gets()
+        {
+            var users = _context.User;
+            return JsonConvert.SerializeObject(users);
+            //return Content("OK");
+        }
     }
 }
