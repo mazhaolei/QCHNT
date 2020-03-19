e2ae280109971e195ce5c582aa442eabc47c41b6
mazhaolei <403085447@qq.com>
2020-03-19 11:49:13 +0800

    类名修改

---
+++ QCHNT/Result/JsonResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QCHNT.Result
+{
+    public partial class JsonResponse
+    {
+        /// <summary>
+        ///  错误信息
+        /// </summary>
+        public string Msg { get; set; }
+        /// <summary>
+        /// 错误代码
+        /// </summary>
+        public ErrorCode Status { get; set; }
+    }
+    public enum ErrorCode
+    {
+        Unknown = -1,
+        Sucess = 0,
+    }
+}
